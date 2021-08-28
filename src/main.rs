e67dcfceccc2d516460d5d90c2fc1d21c1ecd413
Jose Storopoli <thestoropoli@gmail.com>
2021-08-28 09:16:37 -0300

    update fundamental error

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use polars::prelude::*;
 use std::fs::File;
 
 struct Config<'a> {
+    name: String,
     cols: &'a [&'a str],
     delimiter: u8,
 }
@@ -20,17 +21,18 @@ fn read_file(filepath: &str, config: &Config) -> Result<DataFrame> {
         .infer_schema(None)
         .has_header(true)
         .finish()?;
+    println!("{} file has: {} records", config.name, df.shape().0);
     Ok(df)
 }
 
 fn write_file(df: &DataFrame, filepath: &str) -> Result<()> {
+    println!("Combined records file has: {} records", df.shape().0);
     let mut file = File::create(&filepath).expect("could not create file");
     // write DataFrame to file
     CsvWriter::new(&mut file)
         .has_headers(true)
         .with_delimiter(b',')
         .finish(&df)
-
 }
 
 /// Program to combine data from Scopus and Web of Science by DOI
@@ -55,21 +57,32 @@ fn main() {
 
     // Config Structs
     let config_scopus = Config {
+        name: String::from("Scopus"),
         cols: &["Authors", "Title", "Source title", "DOI"],
         delimiter: b',',
     };
-    let config_wos = Config{
+    let config_wos = Config {
+        name: String::from("Web of Science"),
         cols: &["AU", "TI", "SO", "DI"],
         delimiter: b'\t',
     };
 
     // Get Data as DataFrames
-    let mut scopus = read_file(rsl.scopus.as_str(), &config_scopus).expect("Could not read Scopus CSV file path");
-    let wos = read_file(rsl.wos.as_str(), &config_wos).expect("Could not read Web of Science file path");
+    let mut scopus = read_file(rsl.scopus.as_str(), &config_scopus)
+        .expect("Could not read Scopus CSV file path");
+    let wos =
+        read_file(rsl.wos.as_str(), &config_wos).expect("Could not read Web of Science file path");
 
-    scopus.set_column_names(&["AU", "TI", "SO", "DI"]).expect("Cannot rename Scopus Columns");
-    scopus.outer_join(&wos, "DI", "DI").expect("Could not join datasets");
+    // Normalize Columns
+    scopus
+        .set_column_names(&["AU", "TI", "SO", "DI"])
+        .expect("Cannot rename Scopus Columns");
 
-    write_file(&scopus, rsl.output.as_str()).expect("Could not save combined file");
+    // Outer Join Scopus and WoS
+    let df = scopus
+        .outer_join(&wos, "DI", "DI")
+        .expect("Could not join datasets");
+
+    write_file(&df, rsl.output.as_str()).expect("Could not save combined file");
     println!("Done!");
 }
